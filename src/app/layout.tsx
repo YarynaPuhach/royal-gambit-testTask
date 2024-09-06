6296bed0c4ca79557d6ec2b159bc0bf93e45393e
Yaryna <puhachyaryna@gmail.com>
2024-09-06 17:32:16 +0200

    style fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import { ReactNode, Suspense } from "react";
 import styles from './globals.module.scss';
 import ProductProvider from "./context/ProductsContext";
+import PageSkeleton from "./components/PageSkeleton/PageSkeleton";
 
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={styles.body}>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<PageSkeleton />}>
         <ProductProvider>
             {children}
           </ProductProvider>
